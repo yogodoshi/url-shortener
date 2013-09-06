89b03fd35102b49dcf47984b29af24e7c559e084
Cayo Medeiros (yogodoshi) <yogodoshi@gmail.com>
2013-09-06 07:17:34 -0300

    Implementing Sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Logado com sucesso!"
+      redirect_to root_path
+    else
+      flash[:error] = "Senha ou email errado =/"
+      render "new"
+    end
+  end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -11,4 +11,50 @@ describe SessionsController do
     it { should render_template(:new) }
     it { should render_with_layout(:application) }
   end
+
+
+  describe "POST 'create'" do
+    let!(:user) { create(:user, email: "contato@dobom.com", password: "1234", password_confirmation: "1234") }
+
+    context "with valid params" do
+      let(:params) do
+        {
+          email: "contato@dobom.com",
+          password: "1234"
+        }
+      end
+
+      it "should redirect to root_path" do
+        post :create, params
+        should redirect_to(root_path)
+      end
+
+      it "should set the flash correctly" do
+        post :create, params
+        should set_the_flash[:success].to("Logado com sucesso!")
+      end
+
+      it "should set the session" do
+        expect do
+          post :create, params
+        end.to change{ session[:user_id] }.from(nil).to(user.id)
+      end
+    end
+
+    context "with invalid params" do
+      let(:params) do
+        { email: "contato@dobom.com" }
+      end
+
+      it "should set the flash correctly" do
+        post :create, params
+        should set_the_flash[:error].to("Senha ou email errado =/")
+      end
+
+      it "should re-render the 'new' template" do
+        post :create, params
+        should render_template("new")
+      end
+    end
+  end
 end
