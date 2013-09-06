b0a57cd65d79ed3ce53444b144ab26a58c59f12b
Cayo Medeiros (yogodoshi) <yogodoshi@gmail.com>
2013-09-06 08:19:16 -0300

    Implementing route for Users#urls

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ UrlShortener::Application.routes.draw do
 
   resources :urls, only: [:create, :new]
   resources :users, only: [:create, :new]
+  get "urls", to: "users#urls", as: :user_urls
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   resources :sessions, only: [:create, :new]
-  get "/:id", to: "urls#unshorten", as: :unshorten_url
+  get ":id", to: "urls#unshorten", as: :unshorten_url
 end
--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -7,6 +7,10 @@ describe UsersController do
       expect(get("/users/new")).to route_to("users#new")
     end
 
+    it "routes to #urls" do
+      expect(get("/urls")).to route_to("users#urls")
+    end
+
     it "routes to #create" do
       expect(post("/users")).to route_to("users#create")
     end
@@ -15,6 +19,10 @@ describe UsersController do
   describe "route helpers" do
     it "new_user_path" do
       expect(new_user_path).to eq("/users/new")
+    end
+
+    it "user_urls_path" do
+      expect(user_urls_path).to eq("/urls")
     end
   end
 end
