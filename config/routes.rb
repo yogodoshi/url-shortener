72ffff55b86430653f24c8e1cc8927761a95578d
Cayo Medeiros (yogodoshi) <yogodoshi@gmail.com>
2013-09-06 05:38:48 -0300

    Implementing routes for UrlsController#unshorten

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ UrlShortener::Application.routes.draw do
   root :to => "urls#new"
 
   resources :urls, only: [:create, :new]
+  get "/:id", to: "urls#unshorten", as: :unshorten_url
 end
--- spec/routing/urls_routing_spec.rb
+++ spec/routing/urls_routing_spec.rb
@@ -10,6 +10,10 @@ describe UrlsController do
     it "routes to #create" do
       expect(post("/urls")).to route_to("urls#create")
     end
+
+    it "routes to #unshorten" do
+      expect(get("/1234")).to route_to("urls#unshorten", id: "1234")
+    end
   end
 
   describe "route helpers" do
@@ -23,6 +27,10 @@ describe UrlsController do
 
     it "urls_path" do
       expect(urls_path).to eq("/urls")
+    end
+
+    it "unshorten_url_path" do
+      expect(unshorten_url_path("1234")).to eq("/1234")
     end
   end
 end
