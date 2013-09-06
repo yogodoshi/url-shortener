71fda4a24e1ccfe498db9a6a4bf2a699d4218c29
Cayo Medeiros (yogodoshi) <yogodoshi@gmail.com>
2013-09-06 06:29:24 -0300

    Implementing Users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Pronto, agora você está logado!"
+      redirect_to root_path
+    else
+      render "new"
+    end
+  end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -14,4 +14,59 @@ describe UsersController do
       expect(assigns(:user)).to be_a(User)
     end
   end
+
+  describe "POST 'create'" do
+    context "with valid params" do
+      let(:params) do
+        {
+          user: {
+            email: "email@avassalador.com",
+            password: "1234",
+            password_confirmation: "1234"
+          }
+        }
+      end
+
+      it "should assign @user with a User" do
+        post :create, params
+        expect(assigns(:user)).to be_a(User)
+      end
+
+      it "should redirect to root_path" do
+        post :create, params
+        should redirect_to(root_path)
+      end
+
+      it "should set the flash correctly" do
+        post :create, params
+        should set_the_flash[:success].to("Pronto, agora você está logado!")
+      end
+    end
+
+    context "with invalid params" do
+      let(:params) do
+        {
+          user: {
+            email: "email@aindacerto.com"
+          }
+        }
+      end
+
+      it "should assign @user with a User" do
+        post :create, params
+        expect(assigns(:user)).to be_a(User)
+      end
+
+      it "should re-render the 'new' template" do
+        post :create, params
+        should render_template("new")
+      end
+
+      it "should not create a new user" do
+        expect do
+          post :create, params
+        end.to_not change{ User.count }
+      end
+    end
+  end
 end
