0846b32f74c07ddd3bc2c1e3521c3f27b6f59b91
Cayo Medeiros (yogodoshi) <yogodoshi@gmail.com>
2013-09-06 06:19:55 -0300

    Adding email uniqueness validation to User

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    email "teste@teste.com"
+    password "123deoliveira4"
+    password_confirmation "123deoliveira4"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,28 @@ describe User do
   describe "authentication" do
     it { should have_secure_password }
   end
+
+  describe "mass assignments" do
+    context "when is allowed" do
+      [:email, :password, :password_confirmation].each do |attr|
+        it { should allow_mass_assignment_of(attr) }
+      end
+    end
+
+    context "when its not allowed" do
+      it { should_not allow_mass_assignment_of(:password_digest) }
+    end
+  end
+
+  describe "validations" do
+    describe "email" do
+      let!(:user){ create(:user, email: "aaa@bbb.com") }
+      let!(:user2){ build(:user, email: "aaa@bbb.com") }
+
+      it "should validate uniqueness of email " do
+          user2.save
+          expect(user2).to validate_uniqueness_of :email
+      end
+    end
+  end
 end
